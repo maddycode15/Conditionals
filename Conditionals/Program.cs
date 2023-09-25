269d8a1614c0e15be530ead4c1a3620895112a52
madeline allison restin <144450180+maddycode15@users.noreply.github.com>
2023-09-25 08:50:16 -0300

    Initial commit

---
+++ Conditionals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("conditionals");
+            Console.WriteLine("---------");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("");
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
