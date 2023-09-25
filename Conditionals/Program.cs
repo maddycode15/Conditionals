ab9576352f678d950a2cb1913e1a0f3df4066882
madeline allison restin <144450180+maddycode15@users.noreply.github.com>
2023-09-25 09:36:29 -0300

    working HUD

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -7,17 +7,48 @@ using System.Threading.Tasks;
 namespace Conditionals
 {
     internal class Program
-    {
+    { 
+        
+    
+        static int health;
+        static int damage;
+    
+
+    
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("conditionals");
             Console.WriteLine("---------");
             Console.WriteLine("");
+            // intialisations
 
+            health = 100;
+            damage = 10;
+
+           
+
+       
+                // sample gameplay
+            ShowHUD();
+            TakeDamage(10);
+            ShowHUD();
+      
 
             Console.WriteLine("");
             Console.WriteLine("press any key to exit");
             Console.ReadKey(true);
         }
-    }
+        static void TakeDamage(int v)
+        {
+            Console.WriteLine("player took" + " " + damage + " " + "damage");
+            health = health - damage;
+        }
+
+        static void ShowHUD()
+        {
+            Console.WriteLine("player health" + " " + health);
+        }
+    }   
 }
